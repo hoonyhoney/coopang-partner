5775605d8dad9fc0d7f2f4e5a7a33a11509fcd59
hoonyhoney <hoonyhoeny@gmail.com>
2024-07-28 13:22:50 +0900

    init project

---
+++ src/test/java/com/coopang/partner/PartnerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.coopang.partner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("테스트");
+	}
+
+}
