34bf3f43192875f0536d061dddf18cab687e9ae3
hoonyhoney <hoonyhoeny@gmail.com>
2024-08-01 23:10:29 +0900

    feat: 게시글 자동 작성 테스트 완료

--- src/main/java/com/coopang/partner/service/InstaPostService.java
+++ src/main/java/com/coopang/partner/service/InstaPostService.java
@@ -30,6 +30,7 @@ public class InstaPostService {
        WebElement usernameField = driver.findElementByName("username");
        WebElement passwordField = driver.findElementByName("password");
        usernameField.sendKeys(username);
+       passwordField.sendKeys(password);
 
        //로그인 버튼 클릭
        WebElement loginButton = driver.findOneByXpath("//button[@type='submit']");
@@ -39,26 +40,32 @@ public class InstaPostService {
        driver.wait(5000);
 
        //"+" 버튼 클릭하여 게시물 작성 페이지로 이동
-       WebElement newPostButton = driver.findOneByCss("svg[aria-label='New Post']");
+       WebElement newPostButton = driver.findOneByCss("svg[aria-label='새로운 게시물']");
        newPostButton.click();
 
        //파일 업로드 url을 이미지로 변경
        WebElement fileInput = driver.findOneByXpath("//input[@type='file']");
-       fileInput.sendKeys("/path/to/your/image.jpg");
+       fileInput.sendKeys("F:/images/madmax.jpg");
 
        //다음 버튼 클릭
-       WebElement nextButton = driver.findOneByXpath("//button[contains(text(),'Next')]");
+       WebElement nextButton = driver.findOneByXpath("/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div");
+       //WebElement nextButton = driver.findOneByXpath("//div[contains(text(),'다음')]");
        nextButton.click();
 
+       driver.wait(2000);
+       WebElement nextButton2 = driver.findOneByXpath("/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div");
+       nextButton2.click();
+
        //캡션 입력
-       WebElement captionField = driver.findOneByTag("textarea");
-       captionField.sendKeys("your caption here");
+       WebElement captionField = driver.findOneByCss("div[aria-label='문구를 입력하세요...']");
+       captionField.sendKeys("test");
 
        //공유 버튼 클릭
-       WebElement shareButton = driver.findOneByXpath("//button[contains(text(),'Share')]");
+       WebElement shareButton = driver.findOneByXpath("/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div");
+       //WebElement shareButton = driver.findOneByXpath("//div[contains(text(),'공유하기')]");
        shareButton.click();
      }finally {
-       driver.quit();
+       //driver.quit();
      }
    }
 }
